e22b2fcbfd03483d0b8cd72cdd1c58b76647f9b8
Lilly Daniell <daniell.lilly@gmail.com>
2023-03-08 14:55:56 +0000

    Create purple square on collision

--- script.js
+++ script.js
@@ -5,28 +5,35 @@ const ctx = canvas.getContext("2d");
 const WIDTH = 600;
 const HEIGHT = 600;
 
-
 let redSquarePosition = {
-  left: 75,
+  left: 80,
   top: 100,
 }
-
 let blueSquarePosition = {
-  left: 450,
+  left: 400,
   top: 100,
 }
+let purpleSquarePosition = blueSquarePosition;
+
 
 function drawRedSquare() {
   ctx.fillStyle = "red";
-  ctx.fillRect(redSquarePosition.left, redSquarePosition.top, 75, 75);
+  ctx.fillRect(redSquarePosition.left, redSquarePosition.top, 80, 80);
   // left, top, width, height
 }
-
 function drawBlueSquare() {
   ctx.fillStyle = "blue";
-  ctx.fillRect(blueSquarePosition.left, blueSquarePosition.top, 75, 75);
+  ctx.fillRect(blueSquarePosition.left, blueSquarePosition.top, 80, 80);
 }
-
+function drawPurpleSquare() {
+  ctx.fillStyle = "purple";
+  ctx.fillRect(purpleSquarePosition.left, purpleSquarePosition.top, 80, 80)
+}
+function drawPurpleWin() {
+  ctx.font = "30px Rubik Iso";
+  ctx.fillStyle = "lime"
+  ctx.fillText("You made purple!", 10, 50);
+}
 drawRedSquare()
 drawBlueSquare()
 
@@ -42,13 +49,20 @@ function listener(event) {
     drawRedSquare()
     drawBlueSquare()
   }
-  // When I press the L key, move the blue shape to the left
+  // When I press the H key, move the blue shape to the left
   if (event.code === 'KeyH') {
     blueSquarePosition.left -= 10
     ctx.clearRect(0, 0, WIDTH, HEIGHT)
     drawRedSquare()
     drawBlueSquare()
   }
+  // When red square hits blue square delete both and create a purple square + text
+  if (blueSquarePosition.left === redSquarePosition.left) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT)
+    drawPurpleSquare()
+    drawPurpleWin()
+  }
+
 }
 
 window.addEventListener('keydown', listener) 
